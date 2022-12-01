31f95d89744ef32d2a133fad576fef43e92b9be7
Chernisheva Elena <elena.chernisheva@inlinegroup-c.ru>
2022-12-01 15:27:32 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        float sideA = readSideLength("A");
+        float sideB = readSideLength("B");
+        float sideC = readSideLength("C");
+
+        try
+        {
+            Triangle triangle = new Triangle(sideA, sideB, sideC);
+
+            printHeightsOfTriangle(triangle);
+        }
+        catch (IllegalArgumentException e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+    private static float readSideLength(String sideName)
+    {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.printf("Введите длину стороны %s:\n",sideName);
+
+        return scan.nextFloat();
+    }
+
+    private static void printHeightsOfTriangle(Triangle triangle)
+    {
+        System.out.printf("Высота стороны A = %.2f\n", triangle.calculateTriangleHeight(triangle.getSideA()));
+        System.out.printf("Высота стороны B = %.2f\n", triangle.calculateTriangleHeight(triangle.getSideB()));
+        System.out.printf("Высота стороны C = %.2f\n", triangle.calculateTriangleHeight(triangle.getSideC()));
+
+    }
+}
---
+++ src/Triangle.java
@@ -0,0 +1,50 @@
+public class Triangle
+{
+    private float sideA;
+    private float sideB;
+    private float sideC;
+
+    public Triangle(float sideA, float sideB, float sideC)
+    {
+        if ((sideA < 0) || (sideB < 0) || (sideC < 0)) throwSideLengthMustBePositive();
+
+        if ((sideA + sideB <= sideC) || (sideB + sideC <= sideA) || (sideC + sideA) <= sideB) throwTriangleDoesNotExist();
+
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    }
+
+    public float getSideA()
+    {
+        return sideA;
+    }
+
+    public float getSideB()
+    {
+        return sideB;
+    }
+
+    public float getSideC()
+    {
+        return sideC;
+    }
+
+    public float calculateTriangleHeight(float calculatedHeightSide)
+    {
+        float semiperimeter = (sideA + sideB + sideC) / 2;
+
+        return 2 * (float) Math.sqrt(semiperimeter * (semiperimeter - sideA) * (semiperimeter - sideB) * (semiperimeter - sideC)) / calculatedHeightSide;
+    }
+
+    private void throwSideLengthMustBePositive()
+    {
+        throw new IllegalArgumentException("Длина стороны треугольника должна быть больше 0!");
+    }
+
+    private void throwTriangleDoesNotExist()
+    {
+        throw new IllegalArgumentException("Из данных сторон нельзя составить треугольник!");
+    }
+
+}
